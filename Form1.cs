d4be696b206f360ebc91aba3146bc49759b5fb4b
Opilane TTHK <opilane@tthk.ee>
2022-02-08 11:19:54 +0200

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tekstiredaktor_Tsõbirev
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog OpenDlg = new OpenFileDialog();
+        SaveFileDialog SaveDlg = new SaveFileDialog();
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (SaveDlg.ShowDialog() == DialogResult.OK)
+            {
+                SaveDlg = new SaveFileDialog();
+                //save.FileName = poster_txt.Text;
+                SaveDlg.Filter = "txt(*.txt)";
+
+                StreamWriter Writer = new StreamWriter(SaveDlg.FileName);
+                for (int i = 0; i < listBox3.Items.Count; i++)
+                {
+                    Writer.WriteLine((string)listBox3.Items[i]);
+                }
+
+                Writer.Close();
+            }
+            SaveDlg.Dispose();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OpenDlg.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader Reader = new StreamReader(OpenDlg.FileName, Encoding.Default);
+                richTextBox1.Text = Reader.ReadToEnd();
+                Reader.Close();
+            }
+            OpenDlg.Dispose();
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            listBox2.Items.Clear();
+
+            listBox1.BeginUpdate();
+
+            string[] Strings = richTextBox1.Text.Split(new char[] { '\n', '\t',' '},
+            StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in Strings)
+            {
+                string Str = s.Trim();
+
+                if (Str == String.Empty) continue;
+                if (radioButton1.Checked) listBox1.Items.Add(Str);
+                if(radioButton2.Checked)
+                {
+                    if (Regex.IsMatch(Str, @"\d")) listBox1.Items.Add(Str);
+                }
+                if(radioButton3.Checked)
+                {
+                    if (Regex.IsMatch(Str, @"\w+@\w+\.\w+")) listBox1.Items.Add(Str);
+                }
+                listBox1.EndUpdate();
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            listBox3.Items.Clear();
+
+            string Find = textBox1.Text;
+            if(checkBox1.Checked)
+            {
+                foreach(string String in listBox1.Items)
+                {
+                    if (String.Contains(Find)) listBox3.Items.Add(String);
+                }
+            }
+            if (checkBox2.Checked)
+            {
+                foreach (string String in listBox2.Items)
+                {
+                    if (String.Contains(Find)) listBox3.Items.Add(String);
+                }
+            }
+        }
+    }
+}
